45a48d4975ec3a980a04a1b610edae7bdf150b6e
divyadharshinigomathinayagam <dd.nayagam95@gmail.com>
2016-09-11 15:08:11 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int a,b,c;
+printf("enter three number");
+scanf("%d%d%d",&a,&b,&c);
+
+if(a>b && a>c)
+printf("%d",a);
+else if(b>c)
+{
+printf("%d",b);
+}
+else
+printf("%d",c);
+}

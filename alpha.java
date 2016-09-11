7ca9786c487d781a8c994432e92de7317e10ba78
divyadharshinigomathinayagam <dd.nayagam95@gmail.com>
2016-09-11 14:43:17 +0530

    Update alpha.java

--- alpha.java
+++ alpha.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*;
 class alpha
 {
-public static void main(String args[]);
+public static void main(String args[])
+{
 char b;
 Scanner sc=new Scanner(System.in);
 System.out.print("enter input");

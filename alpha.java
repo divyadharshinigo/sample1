2d25dc01a1b504d81c1d2af01e7953b7804c45e7
divyadharshinigomathinayagam <dd.nayagam95@gmail.com>
2016-09-11 14:37:55 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class alpha
+{
+public static void main(String args[]);
+char b;
+System.out.print("enter input");
+Scanner sc=new Scanner(System.in);
+b=sc.next();
+
+
+if(b>=65 && b<=91 || b>=97&&b<=122 )
+{
+System.out.print("it is a alphabet");
+}
+else
+System.out.print("not an alphabet");
+}
+}
